d42f944bc5923abffa1df3f807d62dc8d7e0867c
Mikee1705 <dizon2295@gmail.com>
2025-11-24 17:21:26 +0800

    Created folders for customer, rider, and vendor

---
+++ php/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost:3310";
+$username = "root";
+$password = "";
+$database = "Food_Delivery";
+
+$conn = new mysqli(
+    $servername, 
+    $username, 
+    $password, 
+    $database);
+
+?>
