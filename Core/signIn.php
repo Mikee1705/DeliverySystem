5594e9a5ebdf7985525566943119d006aac565bc
Mikee1705 <dizon2295@gmail.com>
2025-11-25 10:26:22 +0800

    Modified index.html and redirect.php... Added core/signIn.php

---
+++ Core/signIn.php
@@ -0,0 +1,14 @@
+<h1>Welcome again to QuickBites!</h1>
+    <form action="/php/redirect.php" method="POST">
+        <fieldset>
+            <legend>Enter your information</legend>
+            <label for="email">Email: </label>
+            <input type="text" name="email"><br>
+            <label for="password">Password: </label>
+            <input type="password" name="password"><br>
+            <label for="password">Enter Password again: </label>
+            <input type="password" name="password"><br>
+        </fieldset>
+        <br>
+        <button type="submit">Sign In</button>
+    </form>
