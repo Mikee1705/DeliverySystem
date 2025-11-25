bd11817be2dfb9ec447d8f1da670055179f6d0be
Meguuuri <geloc611@gmail.com>
2025-11-25 11:17:43 +0800

    with all cases

--- Vendor/menu_functions.php
+++ Vendor/menu_functions.php
@@ -3,7 +3,7 @@ $servername = "localhost:3310";
 $username = "root";
 $password = "slippinjimmy23!";
 
-$database = "Food_Delivery";
+$database = "DeliverySystem";
 
 $conn = new mysqli(
     $servername, 
@@ -13,51 +13,47 @@ $conn = new mysqli(
 
 
 if(isset($_POST["add_vendor"])){
-    $id = $_POST["id"] ?? '';
+    
     $contact = $_POST["number"]?? '';
     $location = $_POST["location"]?? '';
-    $ratingvendor = $_POST["rating"]?? '';
+    $rating = $_POST["rating"]?? '';
 
+    $errors = [];
+
+    if($contact === ''){
+        $errors[] = "Phone number is Required";
+    }elseif(!ctype_digit($contact)){
+        $errors[] = "Phone number must be numeric";
+    }elseif(strlen($contact) != 11){
+        $errors[] = "Phone Number must be Exactly 11 digits";
+    }
+
+    if($location === ''){
+        $errors[] = "Location is Required";
+    } elseif(strlen($location) < 3){
+        $errors[] = "Location Must be at least 3 Characters Long";
+    }  elseif (!preg_match('/^[a-zA-Z\s]+$/', $location)) {
+    $errors[] = "Location must contain letters only";
+    }
+    if(!empty($errors)){
+        foreach($errors as $error){
+            echo $error ."<br>";
+        }
+
+    }else{ 
+        if($rating ===''){
+         $sim =  $conn->prepare("INSERT INTO vendor (VEN_PHONE_NUMBER, VEN_LOCATION) 
+            VALUES (?,?)");
+            $sim->bind_param("ss", $contact, $location);
+    }else{
+        $sim =  $conn->prepare("INSERT INTO vendor (VEN_PHONE_NUMBER, VEN_LOCATION, VEN_RATING) 
+        VALUES (?,?,?)");
+
+        $sim->bind_param("sss", $contact, $location, $rating);
+        }
     
-    if(strlen($_POST["id"]) > 8) {
-        echo "ID must be less than 8 characters";
-    
-        if(!isset($_POST["id"])) {
-            echo "ID is Missing";
-            
-        }
-         if(!is_numeric($_POST["id"])) {
-            echo "ID must be a Number";
-        
-        }
+    $sim->execute();
+    echo "Data Added Sucessfully";
     }
-    $search = 0;
-    $searchagain = 9;
-    if(!is_numeric($_POST["number"])) {
-        echo "Phone number must be a Number";
-        
-        if(!in_array($contact, $search) && !in_array($contact, $searchagain) && strlen($_POST["number"]) < 10) {
-            echo "Invalid Phone Number";
-            
-        }
-        if(strlen($_POST["number"]) > 10) {
-            echo "Number must be less than 10 characters";
-        
-        }
-    }
-    if(strlen($_POST["location"]) < 3) {
-            echo "Location must be more than 3 Characters";
-            
-        }
-    if(!isset($_POST["location"])) {
-        echo "Location is Missing";
-        
-     }
-    if(!is_string($_POST["location"])) {
-            echo "Location must be a String";
-            
-    }
-    $conn->query("INSERT INTO vendor (VEN_ID, VEN_PHONE_NUMBER, VEN_LOCATION, VEN_RATING) 
-    VALUES ('$id', '$contact', '$location', '$ratingvendor')");
 }
 ?>
