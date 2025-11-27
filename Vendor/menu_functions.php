2ab35bfe75b44371b62b3c295a1b8aff84fe0119
Meguuuri <geloc611@gmail.com>
2025-11-27 12:43:03 +0800

    updated functions

--- Vendor/menu_functions.php
+++ Vendor/menu_functions.php
@@ -1,16 +1,42 @@
-
-
 <?php
 require_once __DIR__ . '/../database/connect.php';
 
 if(isset($_POST["add_vendor"]) || isset($_POST["Update"])){
-    
+
+    $company = $_POST["company"]?? '';
     $contact = $_POST["number"]?? '';
     $location = $_POST["location"]?? '';
     $rating = $_POST["rating"]?? '';
 
     $errors = [];
 
+    if($company === ''){
+        $errors[] = "Company Name Is Required";
+    }elseif(strlen($company) < 3){
+        $errors[] = "Company Name Must Have More Than 3 Characters";
+    }elseif (!preg_match('/^[a-zA-Z0\s,]+$/', $company)) {
+        $errors[] = "Company Must Contain Strings Only";
+    }
+    //checks for duplicate company names
+    if(empty($errors)){
+        $currentID = $_POST['vencode'] ?? null;
+        if($currentID){
+            $stmt = $conn -> prepare("SELECT VEN_ID FROM VENDOR 
+            WHERE VEN_PHONE_NUMBER = ? AND VEN_ID <> ?");
+            $stmt->bind_param("si", $company, $currentID);
+        }else{
+            $stmt = $conn -> prepare("SELECT VEN_ID FROM VENDOR 
+            WHERE VEN_NAME = ?");
+            $stmt->bind_param("s", $company);
+            
+        } 
+        $stmt->execute();
+        $stmt->store_result();
+        if($stmt->num_rows > 0){
+            $errors[] = "Company Name Already Exists";
+        }
+        $stmt->close();
+    }
     if($contact === ''){
         $errors[] = "Phone number is Required";
     }elseif(!ctype_digit($contact)){
@@ -58,21 +84,22 @@ if(isset($_POST["add_vendor"]) || isset($_POST["Update"])){
             $conn->query(
                 "UPDATE VENDOR 
                  SET VEN_PHONE_NUMBER = '$contact',
-                     VEN_LOCATION = '$location'
+                     VEN_LOCATION = '$location',
+                     VEN_NAME = '$company'
                  WHERE VEN_ID = '$Code'"
             );
             echo "Data Updated Sucessfully";
         }
     } elseif (isset($_POST["add_vendor"])) {
     if($rating ===''){
-         $sim =  $conn->prepare("INSERT INTO vendor (VEN_PHONE_NUMBER, VEN_LOCATION) 
-            VALUES (?,?)");
-            $sim->bind_param("ss", $contact, $location);
+         $sim =  $conn->prepare("INSERT INTO vendor (VEN_NAME,VEN_PHONE_NUMBER, VEN_LOCATION) 
+            VALUES (?,?,?)");
+            $sim->bind_param("sss", $company,$contact, $location);
     }else{
-        $sim =  $conn->prepare("INSERT INTO vendor (VEN_PHONE_NUMBER, VEN_LOCATION, VEN_RATING) 
-        VALUES (?,?,?)");
+        $sim =  $conn->prepare("INSERT INTO vendor (VEN_NAME,VEN_PHONE_NUMBER, VEN_LOCATION, VEN_RATING) 
+        VALUES (?,?,?,?)");
 
-        $sim->bind_param("sss", $contact, $location, $rating);
+        $sim->bind_param("ssss", $company,$contact, $location, $rating);
             }
             $sim->execute();
             echo "Data Added Sucessfully";
@@ -102,6 +129,7 @@ $display = mysqli_query($conn,"SELECT * FROM VENDOR");
         <table border = "1" cellpadding = "4" cellspacing = "0">
         <tr>
             <th>Vendor ID</th>
+            <th>Company Name</th>
             <th>Vendor Contact No.</th>
             <th>Vendor Address</th>
             <th>Vendor Rating</th>
@@ -109,6 +137,7 @@ $display = mysqli_query($conn,"SELECT * FROM VENDOR");
             <?php while ($row = mysqli_fetch_array($display)) { ?>
             <tr>
                 <td><?php echo htmlspecialchars($row['VEN_ID']); ?></td>
+                <td><?php echo htmlspecialchars($row['VEN_NAME']); ?></td>
                 <td><?php echo htmlspecialchars($row['VEN_PHONE_NUMBER']); ?></td>
                 <td><?php echo htmlspecialchars($row['VEN_LOCATION']); ?></td>
                 <td><?php echo htmlspecialchars($row['VEN_RATING']); ?></td>
@@ -121,6 +150,8 @@ $display = mysqli_query($conn,"SELECT * FROM VENDOR");
             <td>
                 <form method="POST" action="menu_functions.php">
                 <input type="hidden" name="vencode" value="<?php echo $row['VEN_ID']; ?>">
+
+                <input type="text" name="company" value="<?php echo htmlspecialchars($row['VEN_NAME']); ?>">
 
                 <input type="text" name="number" value="<?php echo htmlspecialchars($row['VEN_PHONE_NUMBER']); ?>">
 
